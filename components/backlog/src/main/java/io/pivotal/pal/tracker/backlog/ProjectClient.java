3b43fddec8e0ead495762ba67e88e75f9e32c659
cjbednarzyk <cjbednarzyk01@alumni.amherst.edu>
2017-12-04 16:45:11 -0500

    added circuit breaker

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,18 +1,38 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String endpoint;
+    private ConcurrentHashMap<Long,ProjectInfo> _projectInfoCache;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
+        this._projectInfoCache = new ConcurrentHashMap<>();
+    }
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
+    public ProjectInfo getProject(long projectId) {
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        updateProjectToCache(projectId,project);
+        return project;
     }
 
-    public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return _projectInfoCache.getOrDefault(projectId,null);
+    }
+
+    private void updateProjectToCache(long projectId,ProjectInfo projectInfo) {
+        if(!_projectInfoCache.containsKey(projectId))
+        {
+            _projectInfoCache.put(projectId,projectInfo);
+        } else {
+            _projectInfoCache.replace(projectId,projectInfo);
+        }
     }
 }
